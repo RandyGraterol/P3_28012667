2308742bc54b5df7a58dc7b586fa9506bfd41947
RandyGraterol <soapdelinger@gmail.com>
2023-12-02 14:15:18 +0100

    face terminada

--- controllers.js
+++ controllers.js
@@ -1,12 +1,34 @@
+// -*- coding: utf-8 -*-
 //Librerias y dependencias
 require('dotenv').config();
+const multer = require('multer');
 const http = require('http');
 const express = require('express');
 const app = express();
+const bodyParser= require('body-parser');
+//app.use(bodyParser.urlencoded({extended: true}));
 const path = require('path');
-const baseDatosModels = require('./models/baseDeDatos.js');
+const baseDatos = require('./models/baseDatos.js');
+const utils = require('./utils/uploadImg.js');
 const {contrasena,admin} = process.env;
+let ext;
 let login= false;
+
+
+//--------------------------------------------------------------
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './static/uploads')
+  },
+  filename: function (req, file, cb) {
+    ext = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + Date.now() + utils.getContentType(ext))
+  }
+})
+
+let upload = multer({ storage: storage });
+//---------------------------------------------------------------
+
 //recursos que se van a cargar en el server 
 app.use(express.static(__dirname+'/static'));
 
@@ -15,8 +37,8 @@ app.use(express.static(__dirname+'/static'));
 app.set('view engine','ejs');//definimos el motor de plantilla con archivos ejs
 app.set('views',path.join(__dirname,"./views"));//definimos la ruta del motor de plantilla
 app.use(express.urlencoded({extended:false}));//permite recuperar los valores publicados en un request
-port = app.listen(5000);
-console.log('Servidor corriendo exitosamente en el puerto 5000');
+port = app.listen(3000);
+console.log('Servidor corriendo exitosamente en el puerto 3000');
 
 
 
@@ -51,48 +73,48 @@ res.render('add.ejs');
 });
 
 //---------------------------------------------------------
-app.get('/addImagen',(req,res)=>{
-res.render('addImagen.ejs');
+app.get('/addImagen/:id',(req,res)=>{
+baseDatos.getImagen(req,res);
 });
 
 
-app.post('/addImagen',(req,res)=>{
-baseDatosModels.aggIMG(req,res);
+app.post('/addImagen/:id',upload.single('img'),(req,res)=>{ 
+baseDatos.aggIMG(req,res);
 });
 
 
 app.post('/addPost',(req,res)=>{   
-baseDatosModels.aggDato(req,res);
+baseDatos.aggDato(req,res);
 });
 
 
 app.get('/productos',(req,res)=>{
-  baseDatosModels.mostrarProductos(req,res);
+  baseDatos.mostrarProductos(req,res);
 });
 //-------------------------------------------------------
 // GET /editar/:id
 app.get('/update/:id',(req, res) => {
-baseDatosModels.mostrarUpdate(req,res);
+baseDatos.mostrarUpdate(req,res);
 
 });
 //-------------------------------------------------------
 // POST /editar/:id
 app.post('/update/:id', (req, res) => {
- baseDatosModels.update(req,res);
+ baseDatos.update(req,res);
 });
 //-------------------------------------------------------
 // GET /eliminar/:id
 app.get('/delete/:id', (req, res) => {
- baseDatosModels.mostrarDelete(req,res);
+ baseDatos.mostrarDelete(req,res);
 });
 //-------------------------------------------------------
 // POST /eliminar/:id
 app.post('/delete/:id', (req, res) => {
- baseDatosModels.deletee(req,res);
+ baseDatos.deletee(req,res);
 });
 //------------------------------------------------------
 app.get('/categorias', (req, res) => {
- baseDatosModels.getCategorias(req,res);
+ baseDatos.getCategorias(req,res);
 });
 //-------------------------------------------------------
 app.get('/addCategorias', (req, res) => {
@@ -100,19 +122,36 @@ app.get('/addCategorias', (req, res) => {
 });
 //-------------------------------------------------------
 app.post('/addcategorias', (req, res) => {
- baseDatosModels.postCategorias(req,res);
+ baseDatos.postCategorias(req,res);
 });
 //-------------------------------------------------------
 app.get('/updateCategoria/:id',(req,res)=>{
- baseDatosModels.mostrarUpdateC(req,res);
+ baseDatos.mostrarUpdateC(req,res);
 });
 //-------------------------------------------------------
 app.post('/updateCategoria/:id',(req,res)=>{
-baseDatosModels.updateCateg(req,res);
+baseDatos.updateCateg(req,res);
+});
+//-------------------------------------------------------
+app.get('/eliminarCategoria/:id',(req,res)=>{
+baseDatos.deleteCategoriaGET(req,res);
+})
+//-------------------------------------------------------
+app.get('/clientes',(req,res)=>{
+  console.log('mostrando pagina la cliente!');
+baseDatos.ClientesGET(req,res);
+})
+//-------------------------------------------------------
+app.get('/cliente', (req, res) => {
+ baseDatos.filtrar(req,res);
+});
+//-------------------------------------------------------
+app.get('/detalles/:id',(req,res)=>{
+baseDatos.detalles(req,res);
 });
 //-------------------------------------------------------
 //Metodo para manejar rutas no encontradas
 app.get('/*',(req,res)=>{
-res.render('notfound.ejs')
+res.render('found.ejs')
 });
 //-------------------------------------------------------
