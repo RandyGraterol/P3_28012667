46f6e6cdf420da676f971c4dc807ab6e2f9d88d3
RandyGraterol <soapdelinger@gmail.com>
2024-01-29 21:53:37 -0400

    Update controllers.js
    actualizando recaptvcha

--- controllers.js
+++ controllers.js
@@ -5,7 +5,7 @@ const path = require('path');
 require('dotenv').config();
 var handlebars = require('express-handlebars')
 const Recaptcha = require('express-recaptcha').RecaptchaV2;
-const recaptcha = new Recaptcha('6LcoNy0pAAAAANL1RlKBWgjSPc2mn7qI5Gf59Xo3', '6LcoNy0pAAAAAFtE0DGF2NzXuf9oqN8AmVVYCpWQ');
+const recaptcha = new Recaptcha('6LcueWApAAAAAK_Ke7XPvCpmFZRn4oVDNv2gF222', '6LcueWApAAAAAJmwZKuIz1qaTVhB6084pDMYVXOL');
 const multer = require('multer');
 const http = require('http');
 const express = require('express');
